7253dd837e06a1e6db24ea673f316db5e6b604a3
Strikeeaglechase <Strikeeaglechase@gmail.com>
2023-09-20 18:49:47 -0400

    Generator based decompression

--- src/compression/decompressV3.ts
+++ src/compression/decompressV3.ts
@@ -5,12 +5,6 @@ export function decompressRpcPacketsV3(bytes: number[] | Buffer) {
 	if (bytes.length == 0) return [];
 	const index = new Index();
 
-	function read(amt = 1) {
-		const result = bytes.slice(index.idx, index.idx + amt);
-		index.increment(amt);
-		return result;
-	}
-
 	function readOne() {
 		return bytes[index.plusplus];
 	}
@@ -24,11 +18,6 @@ export function decompressRpcPacketsV3(bytes: number[] | Buffer) {
 	const getStrFromIdx = () => strings[decompressInt(readOne)];
 	for (let i = 0; i < numStrs; i++) {
 		const strLen = readOne();
-		// const str = read(strLen)
-		// 	.map(c => String.fromCharCode(c))
-		// 	.join("");
-		// const strBytes = read(strLen);
-		// strings.push(str);
 		let str = "";
 		for (let j = 0; j < strLen; j++) {
 			str += String.fromCharCode(readOne());
@@ -106,4 +95,100 @@ export function decompressRpcPacketsV3(bytes: number[] | Buffer) {
 	}
 
 	return rpcPackets;
+}
+
+export function* decompressRpcPacketsV3Gen(bytes: number[] | Buffer) {
+	if (bytes.length == 0) return [];
+	const index = new Index();
+
+	function readOne() {
+		return bytes[index.plusplus];
+	}
+
+	const version = readOne();
+	if (debug_decompress) console.log(`Version: ${version}`);
+
+	const numStrs = decompressInt(readOne);
+	if (debug_decompress) console.log(`Str count: ${numStrs}`);
+	const strings: string[] = [];
+	const getStrFromIdx = () => strings[decompressInt(readOne)];
+	for (let i = 0; i < numStrs; i++) {
+		const strLen = readOne();
+		let str = "";
+		for (let j = 0; j < strLen; j++) {
+			str += String.fromCharCode(readOne());
+		}
+		// const str = Buffer.from(read(strLen)).toString("ascii");
+		strings.push(str);
+		if (debug_decompress) console.log(` #${i} - ${str}`);
+	}
+
+	// Get timestamp offset
+	const timestampOffset = exactBytesToNum(bytes, index);
+
+	if (debug_decompress) console.log(`Timestamp offset: ${timestampOffset}`);
+
+	// RPC Packet format: {classNameIdx} {methodNameIdx} {hasId} {idIdx} {arglen} [arg str]
+	// Read rpc packets
+	const numRpcPackets = version == -1 ? decompressInt(readOne) : exactBytesToNum(bytes, index);
+
+	if (debug_decompress) console.log(`RPC Count: ${numRpcPackets}`);
+	for (let i = 0; i < numRpcPackets; i++) {
+		const className = getStrFromIdx();
+		const methodName = getStrFromIdx();
+		const packetFlags = readOne();
+
+		const idIsNum = bitCheck(packetFlags, PacketFlags.IdIsNumber);
+		const hasId = bitCheck(packetFlags, PacketFlags.HasId);
+		const hasTimestamp = bitCheck(packetFlags, PacketFlags.HasTimestamp);
+
+		if (debug_decompress) {
+			console.log(`RPC #${i} at ${index}`);
+			console.log(` - Class: ${className}`);
+			console.log(` - Method: ${methodName}`);
+			console.log(` - idIsNum: ${idIsNum}`);
+			console.log(` - hasId: ${hasId}`);
+			console.log(` - hasTimestamp: ${hasTimestamp}`);
+		}
+
+		// const idIdx = bitCheck(packetFlags, PacketFlags.HasId) ? readOne() : undefined;
+		let id: string | undefined = undefined;
+		if (hasId) {
+			if (idIsNum) {
+				id = decompressInt(readOne).toString();
+			} else {
+				id = getStrFromIdx();
+			}
+		}
+		if (debug_decompress) console.log(` - ID: ${id}`);
+
+		let timestamp: number | undefined = undefined;
+		if (hasTimestamp) timestamp = decompressInt(readOne) + timestampOffset;
+		if (debug_decompress) console.log(` - Timestamp: ${timestamp}`);
+
+		const argLen = decompressInt(readOne);
+		if (debug_decompress) console.log(` - Arg len: ${argLen}`);
+		let args: unknown[] = [];
+		if (bitCheck(packetFlags, PacketFlags.BinBody)) {
+			args = decompressArgs(bytes, index, argLen); // read(argLen)
+		} else {
+			let argStr = "";
+			for (let j = 0; j < argLen; j++) {
+				argStr += String.fromCharCode(readOne());
+			}
+			// const argStr = Buffer.from(read(argLen)).toString("ascii");
+			if (debug_decompress) console.log(` - Arg str: ${argStr}`);
+			args = JSON.parse(argStr);
+		}
+
+		const packet: RPCPacket = {
+			className: className,
+			method: methodName,
+			id: id,
+			timestamp: timestamp,
+			args: args
+		};
+
+		yield packet;
+	}
 }
--- src/compression/vtcompression.ts
+++ src/compression/vtcompression.ts
@@ -7,7 +7,7 @@ export const debug_packet_structure = false;
 import { RPCPacket } from "../rpc.js";
 import { decompressRpcPacketsV1 } from "./decompressV1.js";
 import { decompressRpcPacketsV2 } from "./decompressV2.js";
-import { decompressRpcPacketsV3 } from "./decompressV3.js";
+import { decompressRpcPacketsV3, decompressRpcPacketsV3Gen } from "./decompressV3.js";
 
 export class Index {
 	private value = 0;
@@ -356,16 +356,17 @@ function compressRpcPackets(rpcPackets: RPCPacket[], includeTimestamps: boolean)
 }
 
 type Decompressor = (bytes: number[] | Buffer) => RPCPacket[];
+type GenDecompressor = (bytes: number[] | Buffer) => Generator<RPCPacket, void, unknown>;
 
-const decompressVersions: Decompressor[] = [
-	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-	// @ts-ignore
-	// eslint-disable-next-line @typescript-eslint/no-empty-function
-	() => {},
-	decompressRpcPacketsV1,
-	decompressRpcPacketsV2,
-	decompressRpcPacketsV3
-];
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+const decompressVersions: Decompressor[] = [() => {}, decompressRpcPacketsV1, decompressRpcPacketsV2, decompressRpcPacketsV3];
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+const decompressGenVersions: GenDecompressor[] = [() => {}, () => {}, () => {}, decompressRpcPacketsV3Gen];
 
 function decompressRpcPackets(bytes: number[] | Buffer) {
 	if (bytes.length == 0) return [];
@@ -389,4 +390,30 @@ function decompressRpcPackets(bytes: number[] | Buffer) {
 	}
 }
 
-export { compressRpcPackets, decompressRpcPackets };
+function* genEmpty(): Generator<RPCPacket, void, unknown> {
+	yield;
+}
+
+function decompressRpcPacketsGen(bytes: number[] | Buffer) {
+	if (bytes.length == 0) return genEmpty();
+	const version = bytes[0];
+	if (version < 1 || version >= decompressVersions.length) {
+		console.error(`Invalid version for decompress, expected 1-${decompressVersions.length}, got ${version}`);
+		// console.error(`Data:`);
+		// console.error("[" + bytes.join(",") + "]");
+		return genEmpty();
+	}
+
+	try {
+		const decompressor = decompressGenVersions[version];
+		return decompressor(bytes);
+	} catch (e) {
+		console.error(`Decompression error on packet with ${bytes.length} bytes version ${version}`);
+		console.error(e);
+		console.error(`Data:`);
+		// console.error("[" + bytes.join(",") + "]");
+		return genEmpty();
+	}
+}
+
+export { compressRpcPackets, decompressRpcPackets, decompressRpcPacketsGen };
